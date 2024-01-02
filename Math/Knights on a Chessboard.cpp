74a8c5016f43698bda907bb46d57254dc9a2ff71
Shaon Roy <113965094+shaon508@users.noreply.github.com>
2024-01-02 11:06:59 +0600

    Update Knights on a Chessboard.cpp

--- Math/Knights on a Chessboard.cpp
+++ Math/Knights on a Chessboard.cpp
@@ -1,1 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int TestCase;
+    cin>>TestCase;
+    for(int i=1; i<=TestCase; i++)
+    {
+        int n,m;
+        cin>>n>>m;
+        cout<<"Case "<<i<<": ";
+        if(n==1 || m==1)
+        {
+            cout<<max(n,m)<<endl;
+
+        }
+        else if(n==2 || m==2)
+        {
+            if(max(n,m)%2==1)
+            {
+                cout<<max(n,m)+1<<endl;
+            }
+            else
+            {
+                if(max(n,m)%4==0)
+                {
+                    cout<<max(n,m)<<endl;
+                }
+                else
+                {
+                    cout<<max(n,m)+2<<endl;
+                }
+            }
+        }
+        else if((n*m)%2==0)
+        {
+            cout<<(n*m)/2<<endl;
+        }
+        else
+        {
+            cout<<((n*m)/2)+1<<endl;
+        }
+    }
+    return 0;
+}
 
